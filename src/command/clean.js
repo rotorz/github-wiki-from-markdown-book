46899394bd8923ad5f3edc03c97428b146cc496c
Lea Hayes <lea@rotorz.com>
2017-09-15 19:26:41 +0100

    Use strict mode for javascript source files.

--- src/command/clean.js
+++ src/command/clean.js
@@ -1,6 +1,8 @@
 // Copyright (c) Rotorz Limited. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root.
 
+"use strict";
+
 
 const fs = require("fs");
 const glob = require("glob");
